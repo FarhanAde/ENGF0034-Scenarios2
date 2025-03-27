df06b58446cbdf674da94696a6386b4c80cab344
James Mann <jrhmann@proton.me>
2025-03-27 16:14:10 +0000

    my changes

---
+++ backend/mini_python/src/program.py
@@ -0,0 +1,15 @@
+from ast_statement import Program
+from parser_statement import program_parser
+from program_state import run_statement, ProgramState, Ok
+
+class MiniPythonInterpreter:
+    def __init__(self):
+        pass
+
+    def run_code(self, code: str):
+        program: Program = program_parser.parse(code)
+        program_state = ProgramState()
+        
+        for stmt in program.statements:
+            result = run_statement(stmt, program_state)
+            assert isinstance(result, Ok)
